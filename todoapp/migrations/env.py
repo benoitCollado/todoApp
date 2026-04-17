b56747c0cc76cef91712d90a94a2cff4b1a938b1
benoit <collado.benoit@gmail.com>
2026-04-17 08:22:11 +0000

    modification de la config alembic

--- todoapp/migrations/env.py
+++ todoapp/migrations/env.py
@@ -5,11 +5,12 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from app.models.item import Todo
+
 from alembic import context
 
 from sqlmodel import SQLModel
-from app.models.item import Todo
-from app.core.config import setttings
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
